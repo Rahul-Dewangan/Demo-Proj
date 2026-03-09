563872712a1b5209c8abd5e58d8fdea11070eb71
RahulD <rahuldewangan1702@gmail.com>
2026-03-09 11:41:35 +0530

    1. Project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Demo project to brushup my skills',
+    author='Rahul Dewangan',
+    license='MIT',
+)
